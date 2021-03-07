792c7a5661a206947687d47e1dbae9535a069c16
Chris Birster <christopher.birster@gmail.com>
2021-03-07 00:11:43 -0500

    update with tests and split into lib and main

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,67 @@
+use rand::Rng;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::vec::Vec;
 
+pub fn roll() -> Result<String, Box<dyn std::error::Error>> {
+    let mut rng = rand::thread_rng();
+    let mut dice_roll = String::new();
+    for _ in 0..5 {
+        let num = rng.gen_range(1..7);
+        dice_roll.push_str(&num.to_string());
+    }
+    Ok(dice_roll)
+}
+
+pub fn parse_list<P>(filename: P) -> Result<HashMap<String, String>, Box<dyn std::error::Error>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut map = HashMap::<String, String>::new();
+
+    for line in reader.lines() {
+        if let Ok(l) = line {
+            let split: Vec<&str> = l.split("\t").collect();
+            map.insert(split[0].into(), split[1].into());
+        }
+    }
+    Ok(map)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_roll() -> Result<(), Box<dyn std::error::Error>> {
+        let dice_roll = roll()?;
+        assert_eq!(dice_roll.len(), 5);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_list() -> Result<(), Box<dyn std::error::Error>> {
+        // set up test file
+        let dir = tempdir()?;
+        let file_path = dir.path().join("test-file.txt");
+        let file_path_2 = file_path.clone();
+        let mut file = File::create(file_path)?;
+        write!(file, "123\ttest")?;
+
+        // test fn
+        let content = parse_list(file_path_2)?;
+        assert_eq!(content.get("123").unwrap(), "test");
+
+        // remove artifacts
+        dir.close()?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
-use rand::Rng;
 use std::env;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::Read;
-use std::path::Path;
+use std::vec::Vec;
 
-fn main() -> Result<(), failure::Error> {
-    let mut rng = rand::thread_rng();
-    let mut roll = String::new();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // get 6 rolls
+    let mut rolls = Vec::<String>::new();
+    let mut words = Vec::<String>::new();
     for _ in 0..6 {
-        let num = rng.gen_range(0..6);
-        roll.push_str(&num.to_string());
+        rolls.push(passphrase::roll()?);
     }
-    println!("{:?}", roll);
-    let mut cwd = env::current_dir()?;
-    let mut path = writej!("{}/data/eff_large_word_list_2016_07_18.txt", src);
-    path.push("data/eff_large_word_list_2016_07_18.txt");
-    let lines = parse_list(path)?;
-    Ok(())
-}
 
-fn parse_list<P>(filename: P) -> Result<String, failure::Error>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    let mut reader = BufReader::new(file);
-    let mut contents = String::new();
-    reader.read_to_string(&mut contents)?;
-    Ok(contents)
+    // get word list
+    let path = env::current_dir()?
+        .as_path()
+        .join("data/eff_large_word_list_2016_07_18.txt");
+    let map = passphrase::parse_list(path)?;
+
+    // generate a passphrase
+    for roll in rolls.iter() {
+        if let Some(word) = map.get(roll) {
+            words.push(word.into());
+        }
+    }
+    let passphrase = words.join(" ");
+    println!("{}", passphrase);
+    Ok(())
 }
